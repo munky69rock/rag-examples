80fe0daffeafbbd916328010240350776e72692f
Masayuki Uehara <munky69rock@gmail.com>
2024-05-20 15:49:08 +0900

    update agent script

--- 06_agent.ts
+++ 06_agent.ts
@@ -1,72 +1,120 @@
-import type { ChatPromptTemplate } from "@langchain/core/prompts";
-import { DynamicStructuredTool } from "@langchain/core/tools";
-import { ChatOpenAI } from "@langchain/openai";
-import "dotenv/config";
-import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
-import { pull } from "langchain/hub";
-import { z } from "zod";
+import OpenAI from "openai";
+import type {
+  ChatCompletionMessageParam,
+  ChatCompletionTool,
+} from "openai/resources/index.mjs";
 
-// https://js.langchain.com/docs/modules/agents/quick_start
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"],
+  dangerouslyAllowBrowser: true,
+});
 
-const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
+async function getLocation() {
+  const response = await fetch("https://ipapi.co/json/");
+  const locationData = await response.json();
+  return locationData;
+}
 
-const addTool = new DynamicStructuredTool({
-  name: "add",
-  description: "Add two integers together.",
-  schema: z.object({
-    firstInt: z.number(),
-    secondInt: z.number(),
-  }),
-  func: async ({ firstInt, secondInt }) => {
-    return (firstInt + secondInt).toString();
+async function getCurrentWeather(latitude: string, longitude: string) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=apparent_temperature`;
+  const response = await fetch(url);
+  const weatherData = await response.json();
+  return weatherData;
+}
+
+const tools: Array<ChatCompletionTool> = [
+  {
+    type: "function",
+    function: {
+      name: "getCurrentWeather",
+      description: "Get the current weather in a given location",
+      parameters: {
+        type: "object",
+        properties: {
+          latitude: {
+            type: "string",
+          },
+          longitude: {
+            type: "string",
+          },
+        },
+        required: ["longitude", "latitude"],
+      },
+    },
   },
-});
-
-const multiplyTool = new DynamicStructuredTool({
-  name: "multiply",
-  description: "Multiply two integers together.",
-  schema: z.object({
-    firstInt: z.number(),
-    secondInt: z.number(),
-  }),
-  func: async ({ firstInt, secondInt }) => {
-    return (firstInt * secondInt).toString();
+  {
+    type: "function",
+    function: {
+      name: "getLocation",
+      description: "Get the user's location based on their IP address",
+      parameters: {
+        type: "object",
+        properties: {},
+      },
+    },
   },
-});
+];
 
-const exponentiateTool = new DynamicStructuredTool({
-  name: "exponentiate",
-  description: "Exponentiate the base to the exponent power.",
-  schema: z.object({
-    base: z.number(),
-    exponent: z.number(),
-  }),
-  func: async ({ base, exponent }) => {
-    return (base ** exponent).toString();
+const availableTools = {
+  getCurrentWeather,
+  getLocation,
+};
+
+const messages: Array<ChatCompletionMessageParam> = [
+  {
+    role: "system",
+    content: `You are a helpful assistant. Only use the functions you have been provided with.`,
   },
-});
+];
 
-const model = new ChatOpenAI({
-  model: "gpt-3.5-turbo",
-  temperature: 0,
-  // verbose: true,
-});
+async function agent(userInput: string) {
+  messages.push({
+    role: "user",
+    content: userInput,
+  });
 
-const tools = [addTool, multiplyTool, exponentiateTool];
+  for (let i = 0; i < 5; i++) {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: messages,
+      tools: tools,
+    });
 
-const agent = await createOpenAIToolsAgent({
-  llm: model,
-  tools,
-  prompt,
-});
+    // console.debug(`-- [${i}] response:`, response.choices[0]!.message);
 
-const agentExecutor = new AgentExecutor({
-  agent,
-  tools,
-  verbose: true,
-});
+    const { finish_reason, message } = response.choices[0]!;
 
-await agentExecutor.invoke({
-  input:
-    "Take 3 to the fifth power and multiply that by the sum of twelve and three, then square the whole result",
-});
+    if (finish_reason === "tool_calls" && message.tool_calls) {
+      const functionName = message.tool_calls[0]!.function
+        .name as keyof typeof availableTools;
+      const functionToCall = availableTools[functionName];
+      const functionArgs = JSON.parse(
+        message.tool_calls[0]!.function.arguments,
+      );
+      const functionArgsArr = Object.values(functionArgs) as [string, string];
+      const functionResponse = await functionToCall(...functionArgsArr);
+
+      messages.push({
+        role: "function",
+        name: functionName,
+        content: `
+                The result of the last function was this: ${JSON.stringify(
+                  functionResponse,
+                )}
+                `,
+      });
+    } else if (finish_reason === "stop") {
+      messages.push(message);
+      return message.content;
+    }
+  }
+  return "The maximum number of iterations has been met without a suitable answer. Please try again with a more specific input.";
+}
+
+const response = await agent(
+  "Please suggest some activities based on my location and the weather.",
+);
+
+console.log("response:", response);
+
+// console.log("total messages:", messages);
